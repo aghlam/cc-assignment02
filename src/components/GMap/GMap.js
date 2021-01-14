bbb2afb82e7206654d38aec6020758217480638b
[Matthew Duong] <[s3784450@student.rmit.edu.au]>
2021-01-15 10:52:48 +1100

    Added base map - SearchBox.js is not working, as well as the script at the bottom of index.html

---
+++ src/components/GMap/GMap.js
@@ -0,0 +1,48 @@
+import React, { Component, Fragment } from 'react';
+import GoogleMapReact from 'google-map-react';
+import SearchBox from './SearchBox'
+import './GMap.css'
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+ 
+class GMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: -37.840935,
+      lng: 144.946457
+    },
+    zoom: 11
+  };
+ 
+//Put as a child of <GoogleMapReact>
+//<SearchBox placeholder={"23 Biggs Street"} onPlacesChanged={this.handleSearch} />
+
+  render() {
+    return (
+        <Fragment>
+            <div>
+                <h1>Cafe Map</h1>
+            </div>
+
+            <div id="map">
+                <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyBr0tSPg4hwyEsxxhrRoNuBMFeLh8TQSHU',
+                                    libraries: 'places' }}
+                defaultCenter={this.props.center}
+                defaultZoom={this.props.zoom}
+                >
+
+                <AnyReactComponent
+                    lat={-37.840935}
+                    lng={144.946457}
+                    text="My Marker"
+                />
+                </GoogleMapReact>
+            </div>
+        </Fragment>
+      
+    );
+  }
+}
+ 
+export default GMap;
---
+++ src/components/GMap/SearchBox.js
@@ -0,0 +1,30 @@
+//DOES NOT WORK CURRENTLY
+
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
+
+const google = window.google;
+
+export default class SearchBox extends Component {
+  static propTypes = {
+    placeholder: PropTypes.string,
+    onPlacesChanged: PropTypes.func
+  }
+  render() {
+    return <input ref="input" placeholder={this.props.placeholder} type="text"/>
+  }
+  onPlacesChanged = () => {
+    if (this.props.onPlacesChanged) {
+      this.props.onPlacesChanged(this.searchBox.getPlaces());
+    }
+  }
+  componentDidMount() {
+    var input = ReactDOM.findDOMNode(this.refs.input)
+    this.searchBox = new google.maps.places.SearchBox(input);
+    this.searchBoxListener = this.searchBox.addListener('places_changed', this.onPlacesChanged);
+  }
+  componentWillUnmount() {
+    google.maps.event.removeListener(this.searchBoxListener);
+  }
+}
